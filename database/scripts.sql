38a2bc3c6feec9b77d133eb10359202f6b007e49
“Evelin <evelinsteiger1@gmail.com>
2024-08-19 15:00:41 -0300

    fix some erros on sql scripts

--- database/scripts.sql
+++ database/scripts.sql
@@ -15,9 +15,14 @@
     release_dates DATETIME,
     favorite BOOLEAN DEFAULT 0 NOT NULL,
     status ENUM('PENDING', 'PLAYING_NOW', 'FINISHED', 'NOT_INTERESTED') DEFAULT 'PENDING',
-    platform_id INTEGER FOREIGN KEY REFERENCES platforms(id) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIME NOT NULL
+	platform_id INTEGER NOT NULL,
+    FOREIGN KEY (platform_id) REFERENCES platforms(id),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
   );
+
+  ALTER TABLE games ADD description TEXT;
+
+  ALTER TABLE games ADD image TEXT;
 
   CREATE TABLE platforms (
     id INTEGER PRIMARY KEY NOT NULL,
@@ -26,16 +31,16 @@
 
 
   -- INSERTS
-  INSERT INTO users ('nome', 'email', 'client_id') VALUES ('Evelin Steiger', 'evelinsteiger1@gmail.com', 'zu9lflw6vehnews02st1s8np1jqqw1');
+  INSERT INTO users (name,email,client_id) VALUES ('Evelin Steiger','evelinsteiger1@gmail.com','zu9lflw6vehnews02st1s8np1jqqw1');
 
-  INSERT INTO platform ('id', 'nome') VALUES ('1', 'Não informado');
-  INSERT INTO platform ('id', 'nome') VALUES ('6', 'PC Windows');
-  INSERT INTO platform ('id', 'nome') VALUES ('7', 'PS1');
-  INSERT INTO platform ('id', 'nome') VALUES ('8', 'PS2');
-  INSERT INTO platform ('id', 'nome') VALUES ('9', 'PS3');
-  INSERT INTO platform ('id', 'nome') VALUES ('11', 'XBOX');
-  INSERT INTO platform ('id', 'nome') VALUES ('12', 'XBOX 360');
-  INSERT INTO platform ('id', 'nome') VALUES ('48', 'PS4');
-  INSERT INTO platform ('id', 'nome') VALUES ('49', 'XBOX One');
-  INSERT INTO platform ('id', 'nome') VALUES ('167', 'PS5');
-  INSERT INTO platform ('id', 'nome') VALUES ('169', 'XBOX Series X|S');
+  INSERT INTO platforms (id,name) VALUES ('1', 'Não informado');
+  INSERT INTO platforms (id,name) VALUES ('6', 'PC Windows');
+  INSERT INTO platforms (id,name) VALUES ('7', 'PS1');
+  INSERT INTO platforms (id,name) VALUES ('8', 'PS2');
+  INSERT INTO platforms (id,name) VALUES ('9', 'PS3');
+  INSERT INTO platforms (id,name) VALUES ('11', 'XBOX');
+  INSERT INTO platforms (id,name) VALUES ('12', 'XBOX 360');
+  INSERT INTO platforms (id,name) VALUES ('48', 'PS4');
+  INSERT INTO platforms (id,name) VALUES ('49', 'XBOX One');
+  INSERT INTO platforms (id,name) VALUES ('167', 'PS5');
+  INSERT INTO platforms (id,name) VALUES ('169', 'XBOX Series X|S');
